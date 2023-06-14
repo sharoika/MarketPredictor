1c6aed98139a9c61842b6cbc390a3c2d13624d59
maksim54e <maximsharoika@gmail.com>
2023-06-14 01:23:15 -0600

    More changes

--- scripts/FinanceScraper.py
+++ scripts/FinanceScraper.py
@@ -11,34 +11,16 @@ def FinanceScraper(ticker):
     investment = yfinance.Ticker(ticker)
     info = investment.info
 
+    print(info)
     # ticker data
     finances["ticker"]=ticker
 
     # bid and ask data
-    finances["bid"]=info["bid"]
-    finances["bidSize"]=info["bidSize"]
-    finances["ask"]=info["ask"]
-    finances["askSize"]=info["askSize"]
-
-    finances["beta"]=info["beta"]
-    finances["trailingPE"]=info["trailingPE"]
-    finances["forwardPE"]=info["forwardPE"]
+    finances["open"]=info["open"]
+    finances["close"]=info["currentPrice"]
     finances["volume"]=info["volume"]
-    finances["regularMarketVolume"]=info["regularMarketVolume"]
-    finances["averageVolume"]=info["averageVolume"]
-    finances["averageVolume10days"]=info["averageVolume10days"]
-    finances["averageDailyVolume10Day"]=info["averageDailyVolume10Day"]
-    finances["bookValue"]=info["bookValue"]
-    finances["priceToBook"]=info["priceToBook"]
-    finances["shortRatio"]=info["shortRatio"]
-    finances["currentPrice"]=info["currentPrice"]
-    finances["targetHighPrice"]=info["targetHighPrice"]
-    finances["targetLowPrice"]=info["targetLowPrice"]
-    finances["targetMeanPrice"]=info["targetMeanPrice"]
-    finances["targetMedianPrice"]=info["targetMedianPrice"]
-    finances["recommendationMean"]=info["recommendationMean"]
-    finances["recommendationKey"]=info["recommendationKey"]
-    finances["numberOfAnalystOpinions"]=info["numberOfAnalystOpinions"]
+    finances["high"]=info["regularMarketDayHigh"]
+    finances["low"]=info["regularMarketDayLow"]
 
     with open(time.strftime("%Y-%m-%d")+'.json', 'w', encoding='utf-8') as f:
         json.dump(finances, f, ensure_ascii=False, indent=4)
--- training/ML-Algorithm.py
+++ training/ML-Algorithm.py
@@ -1,15 +1,102 @@
-import pandas as pd
+import json
 import numpy as np
-
-np.set_printoptions(precision=3, suppress=True)
-
 import tensorflow as tf
-from tensorflow.keras import layers
 
-tweets = pd.read_json("stock_tweets_analyzed.json")
-stock_info = pd.read_json("")
+# Load the JSON data
+with open("master_tainer.json") as inputData:
+    data = json.load(inputData)
 
-tweets_features = np.copy(tweets)
-abalone_labels = tweets_features.pop('Age')
+data = sorted(data, key=lambda item: item["date"])
 
-print(tweets.head())
+# Extract the features and labels from the data
+features = []
+labels = []
+
+for item, next_day_item in zip(data, data[1:]):
+    for stock, next_day_stock in zip(item["stocks"], next_day_item["stocks"]):
+        
+        stock_data = stock["stock"]
+        next_dat_stock_data = next_day_stock["stock"]
+        
+        ticker = stock_data["ticker"]
+
+
+        open_price = float(stock_data["open"])
+
+        if np.isnan(open_price):
+            print("Missing or NaN value found for open_price")
+
+        close_price = float(stock_data["close"])
+
+        if np.isnan(close_price):
+            print("Missing or NaN value found for close_price")
+
+        high_price = float(stock_data["high"])
+
+        if np.isnan(high_price):
+            print("Missing or NaN value found for high_price")
+
+        low_price = float(stock_data["low"])
+
+        if np.isnan(low_price):
+            print("Missing or NaN value found for low_price")
+
+        volume = float(stock_data["volume"])
+
+        if np.isnan(volume):
+            print("Missing or NaN value found for volume")
+
+        tommorow_price_change = float(next_dat_stock_data["close"] - stock_data["close"])
+        print(tommorow_price_change)
+
+        if np.isnan(tommorow_price_change):
+            print("Missing or NaN value found for tommorow_price_change")
+
+        # Filter tweets for the current ticker
+        tweets = [tweet["tweet"] for tweet in item["tweets"] if tweet["tweet"]["ticker"] == ticker]
+
+        # Calculate average polarity and subjectivity
+        polarity = float(np.mean([tweet["polarity"] for tweet in tweets]))
+        subjectivity = float(np.mean([tweet["subjectivity"] for tweet in tweets]))
+
+        if ticker == "NVDA":
+            polarity = 0
+            subjectivity = 0
+
+        if np.isnan(polarity):
+            print("Missing or NaN value found for polarity" + ticker)
+        
+        if np.isnan(subjectivity):
+            print("Missing or NaN value found for subjectivity" + ticker)
+
+        features.append([open_price, close_price, high_price, low_price, polarity, subjectivity])
+        labels.append(tommorow_price_change)
+
+# Convert the lists to numpy arrays
+features = np.array(features)
+labels = np.array(labels)
+
+# normalized_features = (features - np.mean(features, axis=0)) / np.std(features, axis=0)
+
+# Split the data into training and testing sets
+train_size = int(0.8 * len(features))
+train_features, test_features = features[:train_size], features[train_size:]
+train_labels, test_labels = labels[:train_size], labels[train_size:]
+
+# Create a TensorFlow model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(6,)),
+    tf.keras.layers.Dense(1)
+])
+
+# Compile the model
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss='mean_squared_error')
+
+# Train the model
+model.fit(train_features, train_labels, epochs=1000, batch_size=32)
+
+# Evaluate the model
+mse = model.evaluate(test_features, test_labels)
+print("Mean Squared Error:", mse)
+
+model.save("my_model_test")
